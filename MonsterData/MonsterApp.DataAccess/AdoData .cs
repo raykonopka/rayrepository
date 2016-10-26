21c93a11a550e61bdde24913d1c5e9049b9b341f
raykonopka <konopka.ray@gmail.com>
2016-10-26 16:19:33 -0400

    add project data access

---
+++ MonsterData/MonsterApp.DataAccess/AdoData .cs
@@ -0,0 +1,92 @@
+﻿using MonsterApp.DataAccess;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterApp.DataAccess
+{
+    public class AdoData
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["MonsterDB"].ConnectionString;
+
+
+        #region Methods
+        ///<summary>
+        ///
+        ///</summary>
+        ///<returns></returns>
+        public List<Gender> GetGenders()
+        {
+            var genders = new List<Gender>();
+
+            try
+            {
+                var ds = GetDataDisconnected("SELECT * FROM Monster.Gender;");
+                
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    genders.Add(new Gender()
+                    {
+                        GenderId = int.Parse(row[0].ToString()),
+                        Name = row[1].ToString(),
+                        Active = bool.Parse(row[2].ToString())
+                    });
+                }
+            }
+
+            catch (Exception)
+            {
+                return null;
+            }
+
+            
+            return genders;
+        }
+
+
+        ///<summary>
+        ///
+        ///</summary>
+        ///<returns></returns>
+        public List<MonsterType> GetMonsterTypes()
+        {
+            throw new NotImplementedException("not ready yet");
+        }
+
+
+        ///<summary>
+        ///
+        ///</summary>
+        ///<returns></returns>
+        public List<Title> GetTitles()
+        {
+            throw new NotImplementedException("not ready yet");
+        }
+        #endregion
+
+
+        private DataSet GetDataDisconnected(string query)
+        {
+            SqlDataAdapter da;
+            DataSet ds;
+            SqlCommand cmd;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                cmd = new SqlCommand(qu connection);
+                da = new SqlDataAdapter(cmd);
+                ds = new DataSet();
+
+                da.Fill(ds);
+            }
+
+            return ds;
+        }
+    }
+}
---
+++ MonsterData/MonsterApp.DataAccess/Gender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterApp.DataAccess
+{
+    public class Gender
+    {
+        public int GenderId { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; internal set; }
+
+        ///<summary>
+        ///
+        ///</summary>
+    }
+}
